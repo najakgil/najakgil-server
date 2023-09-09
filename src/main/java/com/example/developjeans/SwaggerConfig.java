d56062fd836a2fbfe063aece6a5b1f1521cbc5f2
hw130 <hewon130@gmail.com>
2023-09-09 22:51:20 +0900

    fix: swagger edit

--- src/main/java/com/example/developjeans/SwaggerConfig.java
+++ src/main/java/com/example/developjeans/SwaggerConfig.java
@@ -14,6 +14,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
@@ -26,6 +27,7 @@ public class SwaggerConfig {
                 .apiInfo(apiInfo());
     }
 
+    @Bean
     public ApiInfo apiInfo(){
         return new ApiInfoBuilder()
                 .title("Dev-gilyeo")
