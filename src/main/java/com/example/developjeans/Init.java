a9619375cbf7747302916bcf51756b9c91ed9ede
hw130 <hewon130@gmail.com>
2024-01-28 00:33:27 +0900

    chore: init 주석 처리

--- src/main/java/com/example/developjeans/Init.java
+++ src/main/java/com/example/developjeans/Init.java
@@ -1,64 +1,64 @@
-package com.example.developjeans;
-
-import com.example.developjeans.entity.Photo;
-import com.example.developjeans.entity.User;
-import com.example.developjeans.global.entity.SocialType;
-import com.example.developjeans.repository.PhotoLikeRepository;
-import com.example.developjeans.repository.PhotoRepository;
-import com.example.developjeans.repository.UserRepository;
-
-import jakarta.annotation.PostConstruct;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.parameters.P;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-
-@Component
-@RequiredArgsConstructor
-@Slf4j
-public class Init {
-
-    private final PhotoRepository photoRepository;
-    private final UserRepository userRepository;
-    private final PhotoLikeRepository photoLikeRepository;
-
-    @PostConstruct
-    private void initFirst(){
+//package com.example.developjeans;
+//
+//import com.example.developjeans.entity.Photo;
+//import com.example.developjeans.entity.User;
+//import com.example.developjeans.global.entity.SocialType;
+//import com.example.developjeans.repository.PhotoLikeRepository;
+//import com.example.developjeans.repository.PhotoRepository;
+//import com.example.developjeans.repository.UserRepository;
+//
+//import jakarta.annotation.PostConstruct;
+//import jakarta.transaction.Transactional;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.core.parameters.P;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//
+//
+//@Component
+//@RequiredArgsConstructor
+//@Slf4j
+//public class Init {
+//
+//    private final PhotoRepository photoRepository;
+//    private final UserRepository userRepository;
+//    private final PhotoLikeRepository photoLikeRepository;
+//
+//    @PostConstruct
+//    private void initFirst(){
 //        initUsers();
-        initPhotos();
-
-    }
-
-    @Transactional
-    public void initUsers() {
-        for (int i = 0; i < 10; i++) {
-            User user = new User();
-            user.setNickName("userNickname" + (i+1));
-            user.setPassword("userPs" + (i+1));
-
-            user.setEmail("user" + (i+1) + "@example.com");
-            user.setKakaoId(Long.parseLong(String.valueOf(i + 1)));
-            user.setRefreshToken("userRefreshToken" + (i+1));
-            userRepository.save(user);
-        }
-    }
-
-    @Transactional
-    public void initPhotos(){
-        List<User> user = userRepository.findAll();
-        if(!user.isEmpty()){
-            for(int i = 0; i < 10; i++){
-                Photo photo = new Photo();
-                photo.setLikes(i + 1);
-                photo.setUser(user.get(i));
-                photo.setImgUrl("https://najakgil" + (i + 1));
-                photoRepository.save(photo);
-            }
-        }
-
-    }
-}
+//        initPhotos();
+//
+//    }
+//
+//    @Transactional
+//    public void initUsers() {
+//        for (int i = 0; i < 10; i++) {
+//            User user = new User();
+//            user.setNickName("userNickname" + (i+1));
+//            user.setPassword("userPs" + (i+1));
+//
+//            user.setEmail("user" + (i+1) + "@example.com");
+//            user.setKakaoId(Long.parseLong(String.valueOf(i + 1)));
+//            user.setRefreshToken("userRefreshToken" + (i+1));
+//            userRepository.save(user);
+//        }
+//    }
+//
+//    @Transactional
+//    public void initPhotos(){
+//        List<User> user = userRepository.findAll();
+//        if(!user.isEmpty()){
+//            for(int i = 0; i < 10; i++){
+//                Photo photo = new Photo();
+//                photo.setLikes(i + 1);
+//                photo.setUser(user.get(i));
+//                photo.setImgUrl("https://najakgil" + (i + 1));
+//                photoRepository.save(photo);
+//            }
+//        }
+//
+//    }
+//}
